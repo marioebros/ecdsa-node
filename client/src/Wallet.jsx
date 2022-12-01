e18f98885e3a060521a9944de2ca6f947c015b41
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-11-30 22:53:22 -0800

    Imp toHex to pass privateKey to gen address

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,12 +1,13 @@
 import server from "./server";
 
 import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
 
 function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
     const privateKey = evt.target.value;
     setPrivateKey(privateKey);
-    const address = secp.getPublicKey(privateKey); 
+    const address = toHex(secp.getPublicKey(privateKey)); 
     setAddress(address);
     if (address) {
       const {
@@ -28,7 +29,7 @@ function Wallet({ address, setAddress, balance, setBalance, privateKey, setPriva
       </label>
 
       <div>
-        Address: {address}
+        Address: {address.slice(0, 20)}...
       </div>
 
       <div className="balance">Balance: {balance}</div>
